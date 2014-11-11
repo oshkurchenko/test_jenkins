99b2a5951b213165e9153f2f3b1a862baf564138
Oleksii Shkurchenko (oso) <Oleksii.Shkurchenko@tobii.com>
2014-11-11 10:42:37 +0200

    tests

---
+++ src/test/test_main.py
@@ -0,0 +1,17 @@
+import unittest
+
+class FirstPart(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test1(self):
+        pass
+    def test2(self):
+        self.assertEqual(True, True)
+
+class LastPart(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test1(self):
+        self.assertEqual(True, True)
+    def test2(self):
+        self.assertEqual(True, False)
